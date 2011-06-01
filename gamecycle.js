9a8bac29fcf8c2a6afaf4827ef1f81ecec4fcfe6
PotHix <pothix@pothix.com>
2011-06-01 00:30:17 -0300

    Fixing syntax warnings pointed by JShint

--- gamecycle.js
+++ gamecycle.js
@@ -7,7 +7,7 @@
  * Gamecycle contains your basic game loop: intro, menus,
  * crossfading between stages/lifes, gameover and ending.
  */
-var gamecycle={
+var gamecycle = {
 
 	/**
 	 * Gamecycle constructor - initializes a new game object
@@ -213,7 +213,7 @@ var gamecycle={
 			switch (this.state) {
 
 				// Disclaimer
-				case 50: {
+				case 50:
 					if (this.stateFirstIteration) {
 						this._resetGroups();
 						this.gameDisclaimerAnimation(true);
@@ -222,18 +222,17 @@ var gamecycle={
 					if (this.gameDisclaimerAnimation(false))
 						this.setState(100);
 					break;
-				}
 				// main menu
 				case 100:
 				case 101:
-				case 102: { // Press Start / Menu
+				case 102: // Press Start / Menu
 					if (this.stateFirstIteration && (this.state == 100)) {
 						this._resetGroups();
 						this.gameTitleIntroAnimation(true);
 					}
 					this.gameTitleIntroAnimation(false);
 					switch (this.state) {
-						case 100: { // Press to start
+						case 100: // Press to start
 							if (this.stateFirstIteration) {
 								gbox.resetChannel("bgmusic");
 								this.pressStartIntroAnimation(true);
@@ -241,8 +240,7 @@ var gamecycle={
 							}
 							if (this.pressStartIntroAnimation(false)) this.setState(101);
 							break;
-						}
-						case 101: { // Game menu
+						case 101: // Game menu
 							if (this.stateFirstIteration) {
 								gbox.setChannelVolume("bgmusic",0.5);
 								this.gameMenu(true);
@@ -253,8 +251,7 @@ var gamecycle={
 							if (menu)
 								if (menu == -1) this.setState(100); else this.setState(102);
 							break;
-						}
-						case 102: { // Fader
+						case 102: // Fader
 							if (this.stateFirstIteration) {
 								this._resetGroups();
 								toys.resetToy(this,"fadeout");
@@ -263,11 +260,8 @@ var gamecycle={
 							if (toys.fullscreen.fadeout(this,"fadeout",gbox.getBufferContext(),{fadespeed:0.05,audiochannelfade:"bgmusic"}))
 								this.setState(200);
 							break;
-						}
-						break;
 					}
 					break;
-				}
 				case 200:// Game intro animation
 
 				case 300:// Start game
@@ -288,10 +282,10 @@ var gamecycle={
 				case 800:// Fade out game ending
 				case 801:// Game ending
 
-				{ // Game playing
+				    // Game playing
 					if (this.stateFirstIteration) {
 						switch (this.state) {
-							case 200: { // Game intro
+							case 200: // Game intro
 								toys.resetToy(this,"fadein");
 								this.level=null;
 								this._nextlevel=null;
@@ -299,129 +293,107 @@ var gamecycle={
 								this.initializeGame();
 								this.gameIntroAnimation(true);
 								break;
-							}
-							case 300: {
+							case 300:
 								// Game start
 								this.level=this._nextlevel;
 								gbox.playAllGroups();
 								this.changeLevel(this._nextlevel);
-							}
+								break;
 							case 800:
-							case 400:{
+							case 400:
 								this.endlevelIntroAnimation(true);
 								toys.resetToy(this,"fadeout");
 								break;
-							}
-							case 501: {
+							case 501:
 								toys.resetToy(this,"fadeout");
 								break;
-							}
-							case 401: {
+							case 401:
 								gbox.soloGroup(this.group);
 								this.levelIntroAnimation(true);
 								break;
-							}
-							case 402: {
+							case 402:
 								toys.resetToy(this,"fadein");
 								this.level=this._nextlevel;
 								gbox.playAllGroups();
 								this.changeLevel(this._nextlevel);
 								break;
-							}
-							case 600: {
+							case 600:
 								gbox.soloGroup(this.group);
 								this.newlifeIntroAnimation(true);
 								break;
-							}
-							case 500: {
+							case 500:
 								this._loselife.counter=0;
 								break;
-							}
-							case 601: {
+							case 601:
 								toys.resetToy(this,"fadein");
 								this.newLife();
 								gbox.playAllGroups();
 								break;
-							}
-							case 700: {
+							case 700:
 								gbox.resetChannel("bgmusic");
 								gbox.soloGroup(this.group);
 								this.gameoverIntroAnimation(true);
 								break;
-							}
-							case 801: {
+							case 801:
 								gbox.resetChannel("bgmusic");
 								gbox.soloGroup(this.group);
 								this.gameEndingIntroAnimation(true);
 								break;
-							}
 						}
 						this.stateIsReady();
 					}
 
 					switch (this.state) {
-						case 200: { // Game intro
+						case 200: // Game intro
 							if (this.gameIntroAnimation(false)) this.setState(300);
 							break;
-						}
 						case 601: // Fade in with new life
 						case 402: // Fade in after level change
-						case 300: { // Fade in at the beginning of the game
+						case 300: // Fade in at the beginning of the game
 							if (toys.fullscreen.fadein(this,"fadein",gbox.getBufferContext(),{fadespeed:0.05,audiochannelfade:"bgmusic"})) this.setState(301);
 							break;
-						}
-						case 301: { // Ingame stuff
+						case 301: // Ingame stuff
 							this.gameEvents();
 							break;
-						}
-						case 400: { // Fade out before changing the level
+						case 400: // Fade out before changing the level
 							if (this.endlevelIntroAnimation(false))
 								if (toys.fullscreen.fadeout(this,"fadeout",gbox.getBufferContext(),{fadespeed:0.05,audiochannelfade:"bgmusic"})) this.setState(401);
 							break;
-						}
-						case 800: { // Fade out before game ending
+						case 800: // Fade out before game ending
 							if (this.endlevelIntroAnimation(false))
 								if (toys.fullscreen.fadeout(this,"fadeout",gbox.getBufferContext(),{fadespeed:0.05,audiochannelfade:"bgmusic"})) this.setState(801);
 							break;
-						}
-						case 501: { // Fade out after dead
+						case 501: // Fade out after dead
 							if (toys.fullscreen.fadeout(this,"fadeout",gbox.getBufferContext(),{fadespeed:0.05,audiochannelfade:"bgmusic"}))
 								if (this.gameIsOver())
 									this.setState(700); // GAME OVER
 								else
 									this.setState(600);
 							break;
-						}
-						case 401:{ // Level intro animation
+						case 401: // Level intro animation
 							if (this.levelIntroAnimation(false)) this.setState(402);
 							break;
-						}
-						case 500: { // Wait after dead
+						case 500: // Wait after dead
 							this._loselife.counter++;
 							if (this._loselife.counter==this._loselife.wait) this.setState(501);
 							break;
-						}
-						case 600:{ // New life intro
+						case 600: // New life intro
 							if (this.newlifeIntroAnimation(false)) this.setState(601);
 							break;
-						}
-						case 700:{ // gameover
+						case 700: // gameover
 							if (this.gameoverIntroAnimation(false)) this.setState(100); // Restart game
 							break;
-						}
-						case 801:{ // Game ending
+						case 801: // Game ending
 							if (this.gameEndingIntroAnimation(false)) {
 								this._loselife={ending:true};
 								this.setState(700); // Game over
 							}
 							break;
-						}
 					}
 					this.hud.blit();
 					break;
 				}
 			}
-		}
-	});
+		});
 	}
-}
+};
