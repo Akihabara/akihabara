251ab5b4fd3b78e75003695f89887b5a97c647d9
Carlos Benitez <info@etnassoft.com>
2012-01-02 12:27:07 +0100

    New added method for debug.js -> Status Bar
    Now, the Status Bar have been refactorized outside core.
    
    Syntax:
    debug.statusBar( [options] );
    
    Where options are:
    - backgroundColor [optional. Default: #FFF]: The background color of the status bar (CSS Syntax).
    - color [optional. Default: #000]: The font color (CSS Syntax).
    - font [optional. Default: 'lighter 10px sans-serif']: The font params (CSS Syntax)
    
    Sample:
    debug.statusBar({
      backgroundColor : 'black',
      color : 'white',
      font : 'bolder 8px serif'
    });
    
    Files involved:
    - debug.js
    - gbox.js
    - help.js

--- src/debug.js
+++ src/debug.js
@@ -9,29 +9,58 @@ var debug = {
 
 	fpsCounterInit : function(data) {
 
-		// Default assigments
-		data 						|| ( data = {} );
-		data.x 					|| ( data.x = 0 );
-		data.y 					|| ( data.y = 0 );
-		data.font 			|| ( data.font = 'lighter 10px sans-serif' );
-		data.color 			|| ( data.color = "#FFF" );
+		// Default options
+		data 						|| ( data 				= {} );
+		data.x 					|| ( data.x 			= 0 );
+		data.y 					|| ( data.y 			= 0 );
+		data.font 			|| ( data.font 		= 'lighter 10px sans-serif' );
+		data.color 			|| ( data.color 	= "#FFF" );
 
 		// Own fpsCounter vars
-		data.frameCount = 0,
-		data.currentFps = 0,
-		data.lastFps = new Date().getTime();
+		data.frameCount 	= 0,
+		data.currentFps 	= 0,
+		data.lastFps 			= new Date().getTime();
 
 		// Setting data to main object game
-		addDebugAction('fpsCounter', data);
+		this.addDebugAction('fpsCounter', data);
+	},
 
-		// Add a new debig utility and its data
-		function addDebugAction( name, data ){
-			gbox._debugTool[name] = data;
+	statusBar : function(data){
+
+		// Default options
+		data.background	|| ( data.backgorund 	= "#FFF" );
+		data.color 			|| ( data.color 			= "#FFF" );
+		data.font 			|| ( data.font 				= 'lighter 10px sans-serif' );
+
+		this.addDebugAction('statusBar', data);
+	},
+
+	setStatBar : function(txt){
+	  if (!gbox._statbar) createStatBar();
+		gbox._statbar.innerHTML = ( txt || "&nbsp" );
+
+		function createStatBar(){
+			var statbar=document.createElement("div");
+			if (gbox._border) statbar.style.border = "1px solid black";
+			statbar.style.margin = "auto";
+			statbar.style.backgroundColor = gbox._debugTool.statusBar.backgroundColor;
+			statbar.style.font = gbox._debugTool.statusBar.font;
+			statbar.style.color = gbox._debugTool.statusBar.color;
+			statbar.style.width = ( gbox._camera.w * gbox._zoom ) + "px";
+			gbox._container.appendChild(statbar);
+			gbox._statbar = statbar;
 		}
 
 	},
 
+	// Add a new debig utility and its data
+	addDebugAction : function ( name, data ){
+			gbox._debugTool[name] = data;
+	},
+
 	run : function(data){
+
+		//console.log('Debug Init: ', data);
 
 		if(data.fpsCounter){
 
@@ -53,6 +82,26 @@ var debug = {
 			ctx.font = fps.font;
 			ctx.fillText( 'FPS: ' + fps.currentFps + '/' + gbox._fps, fps.x, fps.y );
 
+		}
+
+		if(data.statusBar){
+			var statline="Idle: " + gbox._framestart + "/" + gbox._mspf + (gbox._frameskip > 0 ? " (" + gbox._frameskip + "skip)" : "" ) + " | ";
+			var cnt = 0, g = 0;
+			for ( g = 0; g < gbox._groups.length; g++ )
+				if ( gbox._groupplay[gbox._groups[g]] ) {
+					cnt = 0;
+					for (var obj in gbox._objects[gbox._groups[g]]) cnt++;
+					if (cnt) statline+=gbox._groups[g]+"["+cnt+"] ";
+				}
+			cnt = 0;
+			var ply = 0;
+			for (g in gbox._audio.aud)
+				for (var x = 0; x < gbox._audio.aud[g].length; x++) {
+					cnt++;
+					if (!gbox._audio.aud[g][x].paused&&!gbox._audio.aud[g][x].ended) ply++;
+				}
+			statline += "| audio: " + ply + "/" + cnt + ":" + gbox._audioteam;
+			this.setStatBar(statline);
 		}
 
 	}
